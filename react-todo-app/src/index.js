e7f7dcec8d700aaf0f6321d2331bc553ae6cad04
Nguyen <J4330@atc.gov>
2022-06-23 14:39:05 -0700

    toto list with class syntax

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
 //component file
 import TodoContainer from "./components/TodoContainer"
 
+//stylesheet
+import "./App.scss"
 
 ReactDOM.render(
-    <React.StrictMode>
-        <TodoContainer/>
-    </React.StrictMode>, 
-    document.getElementById("root")
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
 )
